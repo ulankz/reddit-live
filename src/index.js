9b0fe2618f33bbd0d693398207404028b2539131
Ulan Assanov <Ulan@users-iMac-2.local>
2018-11-26 16:18:03 +0600

    adds initial source code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+class Reddit extends React.Component{
 
-ReactDOM.render(<App />, document.getElementById('root'));
+  state = {
+    posts:[]
+  };
+  componentDidMount(){
+    axios.get(`https://www.reddit.com/r/reactjs.json`).then(res =>
+    {
+      const new_posts = res.data.data.children.map(obj=>obj.data);
+      this.setState({posts:new_posts});
+    });
+  }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  render(){
+    return (
+        <div className = "container">
+        <h1>r/reactjs</h1>
+        <ul>
+        {this.state.posts.map(post=>{
+          return <li key = {post.id}>
+          <span className = "name">{post.name}</span>
+          <span className = "score">Score{post.score}</span>
+          <p className = "title">{post.title}</p></li>;
+        })}
+        </ul>
+        </div>
+    );
+  }
+
+}
+ReactDOM.render(<Reddit/>,document.getElementById('root'));
